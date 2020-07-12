ec2f1851b6f2e0194905f404ad26f1c0797a7886
spettway <59152981+spettway@users.noreply.github.com>
2020-07-12 15:36:23 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -199,7 +199,7 @@ const Home = (props: HomeProps) => {
 			
 				 <div className="card_amazon shadow-sm">
 					  <h5 className="card_amazon_header">🔥 Featured</h5>
-					  <div className="card-body">
+					  <div className="card_amazon_body">
 						   <h5 className="card-title">Leave a review to win an Amazon gift card!</h5>
 						   <p className="card-text">We know some Amazon bucks go a long way (we're students too, remember 😉). So 1 out of every 20 reviews will receive a $20 Amazon gift card!</p>
 						   <a href="#" className="btn btn-orange">Leave a review</a>
