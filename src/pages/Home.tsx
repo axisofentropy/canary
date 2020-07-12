d7db623e9b606701e3ee10c34d7e275bc48d20f4
spettway <59152981+spettway@users.noreply.github.com>
2020-07-12 15:54:49 -0400

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -204,7 +204,9 @@ const Home = (props: HomeProps) => {
 				  <div className="card_amazon_body">
 					   <h5 className="card-title">Leave a review to win an Amazon gift card!</h5>
 					   <p className="card-text">We know some Amazon bucks go a long way (we're students too, remember 😉). So 1 out of every 20 reviews will receive a $20 Amazon gift card!</p>
-					   <a href="#" className="btn btn-orange">Leave a review</a>
+					   <Button type="primary" onClick={() => navigate("/submit")}>
+							✎ Write a Review
+					   </Button>
 				   </div>
 			</div>
 			
